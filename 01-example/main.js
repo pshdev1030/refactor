1fc989c6420fc5f3b2d309c8bfd25eb623094187
pshdev1030 <pshdev1030@gmail.com>
2023-03-12 22:39:49 +0900

    refactor: amount 계산 로직을 별개의 함수로 분리

--- 01-example/main.js
+++ 01-example/main.js
@@ -1,6 +1,29 @@
 const plays = require("./plays.json");
 const invoices = require("./invoices.json");
 
+function amountFor(perf, play) {
+  let thisAmount = 0;
+  switch (play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy": {
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    }
+    default: {
+      throw new Error(`알 수 없는 장르 : ${play.type}`);
+    }
+  }
+  return thisAmount;
+}
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -13,26 +36,7 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playId];
-    let thisAmount = 0;
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy": {
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      }
-      default: {
-        throw new Error(`알 수 없는 장르 : ${play.type}`);
-      }
-    }
+    let thisAmount = amountFor(perf, play);
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);

3fa41465e7969c8cef224a1302fd0ff7e4bd8428
pshdev1030 <pshdev1030@gmail.com>
2023-03-12 23:12:24 +0900

    refactor: 포인트 계산함수 분리

--- 01-example/main.js
+++ 01-example/main.js
@@ -39,16 +39,21 @@ function statement(invoice, plays) {
     return result;
   }
 
-  for (let perf of invoice.performances) {
-    // 포인트를 적립한다.
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
     volumeCredits += Math.max(perf.audience - 30, 0);
     if ("comedy" === playFor(perf).type)
       volumeCredits += Math.floor(perf.audience / 5);
+    return volumeCredits;
+  }
+
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+
     result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     }})`;
 
-    // 청구 내역을 출력한다
     result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     }석)\n`;

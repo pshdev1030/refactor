347f3a7993a7af089f29a5b911bbf07018a38e57
pshdev1030 <pshdev1030@gmail.com>
2023-03-12 23:39:18 +0900

    refactor: 함수 로직을 최상위로 이동

--- 01-example/main.js
+++ 01-example/main.js
@@ -3,6 +3,15 @@ const invoices = require("./invoices.json");
 
 function statement(invoice, plays) {
   let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
+  for (let perf of invoice.performances) {
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
+      perf.audience
+    }석)\n`;
+  }
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  return result;
+
   function usd(aNumber) {
     return new Intl.NumberFormat("en-us", {
       style: "currency",
@@ -62,16 +71,6 @@ function statement(invoice, plays) {
     }
     return result;
   }
-
-  for (let perf of invoice.performances) {
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    }석)\n`;
-  }
-
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-  return result;
 }
 
 console.log(statement(invoices[0], plays));

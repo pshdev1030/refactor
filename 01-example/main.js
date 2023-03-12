c561bbaf164328e7b7e98724a591fcddfdd3a241
pshdev1030 <pshdev1030@gmail.com>
2023-03-12 23:07:35 +0900

    refactor: 중첩함수를 사용하고 클로져를 사용해서 변수 개수를 줄이기

--- 01-example/main.js
+++ 01-example/main.js
@@ -1,29 +1,6 @@
 const plays = require("./plays.json");
 const invoices = require("./invoices.json");
 
-function amountFor(aPerformance, play) {
-  let result = 0;
-  switch (play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy": {
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    }
-    default: {
-      throw new Error(`알 수 없는 장르 : ${play.type}`);
-    }
-  }
-  return result;
-}
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -34,19 +11,48 @@ function statement(invoice, plays) {
     minimumFractionDigits: 2,
   }).format;
 
+  function playFor(aPerformance) {
+    return plays[aPerformance.playId];
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy": {
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      }
+      default: {
+        throw new Error(`알 수 없는 장르 : ${playFor(aPerformance)}`);
+      }
+    }
+    return result;
+  }
+
   for (let perf of invoice.performances) {
-    const play = plays[perf.playId];
-    let thisAmount = amountFor(perf, play);
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    result += `${play.name}: ${format(thisAmount / 100)} (${perf.audience}})`;
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
+    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+      perf.audience
+    }})`;
 
     // 청구 내역을 출력한다
-    result += `${play.name}: ${format(thisAmount / 100)} (${
+    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     }석)\n`;
-    totalAmount += thisAmount;
+    totalAmount += amountFor(perf);
   }
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;

4c791bd5d70a5d9cf857980c3cdea9f04eaca1e7
pshdev1030 <pshdev1030@gmail.com>
2023-03-12 23:20:31 +0900

    refactor: format 임시변수를 함수로 분리, 적절한 함수명으로 변경

--- 01-example/main.js
+++ 01-example/main.js
@@ -5,11 +5,13 @@ function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat("en-us", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-us", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playId];
@@ -50,12 +52,12 @@ function statement(invoice, plays) {
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 }
